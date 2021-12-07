73f12f3d879fecff1048d7460cd3ce8614ef49f0
LeeCH <79134646+leechh0948@users.noreply.github.com>
2021-12-07 15:18:40 +0900

    Add p5js

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let oxygen = [];
+
+function setup(){
+  createCanvas(600, 600);
+}
+
+function draw(){
+  background(51);
+  
+  if(keyIsPressed){
+    if(keyCode == LEFT_ARROW){
+      oxygen.push(new Oxygen());
+    }
+    else if(keyCode == RIGHT_ARROW){
+      oxygen.pop();
+    }
+  }
+  for (let i = 0; i < oxygen.length; i++){
+    oxygen[i].walk();
+    oxygen[i].display();
+  }
+}
+
+class Oxygen{
+  constructor(){
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+  }
+  
+  walk(){
+    this.vel = createVector(random(-1, 1), random(-1, 1));
+    this.pos.add(this.vel);
+  }
+  
+  display(){
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, 3, 3);
+  }
+}
